9e495382d458206f3bebb9e69dc89f2530a45cca
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-11-24 22:34:20 -0800

    Update Hollywood.js

--- src/Compo/Hollywood.js
+++ src/Compo/Hollywood.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Hollywood() {
     const [data,setData]=useState([])
     useEffect(()=>{
-        axios.get('https://blog-backend-789z.onrender.com/api/store')
+        axios.get('https://blog-backend-789z.onrender.comapi/store/')
       .then(response => {
        
         setData(response.data);
